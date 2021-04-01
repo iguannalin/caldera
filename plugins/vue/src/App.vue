1f1faebf998c3aea009d0f93574a16fa0b392d95
Anna Lin <alin@mitre.org>
2021-03-31 23:46:42 -0700

    Added vue plugin for testing

---
+++ plugins/vue/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app" class="section-profile">
+    <div id="vueapp-sidebar">
+      <button type="button" class="button-success atomic-button" onclick="vueappFunction()">Click me!</button>
+    </div>
+    <Flag msg="This is parent speaking"/>
+  </div>
+</template>
+
+<script>
+import Flag from './components/Flag.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Flag
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
